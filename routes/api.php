34f20e5cbbf86f5e4b919c8325ae3fa5c8d6cbc4
Gesiel Lucas <gesiel.lucas.f@gmail.com>
2024-09-29 09:53:08 -0300

    feat: finance model

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+
+    dd($token);
+    return ['token' => $token->plainTextToken];
+});
